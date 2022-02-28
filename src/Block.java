d033708bc743ccf37a2c1b17a19dc63556fa5c9b
Eshan Chatterjee <eshan2001221@gmail.com>
2022-02-28 17:52:38 +0530

    Completed Blocks

--- src/Block.java
+++ src/Block.java
@@ -1,32 +1,44 @@
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Objects;
 
 public class Block
 {
     public String hash;
     public String previousHash;
-    // Let's say our data is stored in as a simple string
-    private String m_data;
+    public String merkleRoot;
+    // Say our data is a simple String
+    public ArrayList<Transaction> transactions = new ArrayList<>();
     private long m_timeStamp;
     private int m_nonce;
 
 
     // Block Constructor
-    public Block(String _data, String _previousHash)
+    public Block(String _previousHash)
     {
-        this.m_data = _data;
         this.previousHash = _previousHash;
         this.m_timeStamp = new Date().getTime();
+
+        //Making sure we do this after we set the other values.
         this.hash = calculateHash();
     }
 
+
     public String calculateHash()
     {
-        return StringUtils.applySHA256(previousHash + Long.toString(m_timeStamp) + m_data);
+        return StringUtils.applySHA256(previousHash + Long.toString(m_timeStamp) +
+                Integer.toString(m_nonce) +
+                merkleRoot);
     }
+
 
     public void mineBlock(int _difficulty)
     {
-        String target = new String(new char [_difficulty]).replace('\0', '0');
+        merkleRoot = StringUtils.getMerkleRoot(transactions);
+
+        //Create a string with difficulty * "0"
+        String target = StringUtils.getDifficultyString(_difficulty);
+        // new String(new char [_difficulty]).replace('\0', '0');
 
         while(!hash.substring(0, _difficulty).equals(target))
         {
@@ -36,4 +48,30 @@ public class Block
 
         System.out.println("Block Mined!!! : " + hash);
     }
+
+
+    // Add Transactions to this block
+    public boolean addTransaction(Transaction _transaction)
+    {
+        // process transaction and check if valid, unless block is genesis block then ignore.
+
+        if(_transaction == null)
+            return false;
+
+        if(!Objects.equals(previousHash, "0"))
+        {
+            if(!_transaction.processTransaction())
+            {
+                System.out.println("Transaction failed to process. Discarded.");
+                return false;
+            }
+        }
+
+        transactions.add(_transaction);
+        System.out.println("Transaction Successfully added to Block");
+        return true;
+    }
+
+
+
 }
