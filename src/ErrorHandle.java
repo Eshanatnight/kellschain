98bbfadcacd532aced09f879530d4e9f373a3777
Eshan Chatterjee <eshan2001221@gmail.com>
2022-02-28 17:00:07 +0530

    ErrorHandling class updated

--- src/ErrorHandle.java
+++ src/ErrorHandle.java
@@ -1,5 +1,8 @@
 public class ErrorHandle
 {
+    // Prints a LocalizedMessage to the console.
+    // Then Prints the stack trace to the console.
+    // Finally throws a new RuntimeException.
     public static void handleException(Exception e)
     {
         System.out.println("Error: " + e.getLocalizedMessage());
